ad5281a4da9de51630f6b5f983ffa6796eb7cf14
LYR612556 <1057281882@qq.com>
2018-05-22 15:10:58 +0800

    和为S的两个数字

---
+++ Algorithom/src/com/niuke/Niuke43_1.java
@@ -0,0 +1,34 @@
+package com.niuke;
+
+import java.util.ArrayList;
+
+/**
+ * 和为S的两个数字,两数之和
+ * created by liyurong
+ **/
+public class Niuke43_1 {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6};
+        ArrayList<Integer> res = FindNumbersWithSum(array,6);
+        for (int i = 0;i < res.size();i ++){
+            System.out.println(res.get(i));
+        }
+    }
+    public static ArrayList<Integer> FindNumbersWithSum(int [] array, int sum) {
+        int left = 0;
+        int right = array.length - 1;
+        ArrayList<Integer> res = new ArrayList<>();
+        while (left < right){
+            if (array[left] + array[right] == sum){
+                res.add(array[left]);
+                res.add(array[right]);
+                return res;
+            } else if (array[left] + array[right] < sum){
+                left ++;
+            } else {
+                right --;
+            }
+        }
+        return res;
+    }
+}
