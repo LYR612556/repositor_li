ee6fc66f02cb5d7bcc22333177c52c1509e9e001
LYR612556 <1057281882@qq.com>
2018-05-22 16:14:17 +0800

    不用加减和判断语句计算1+2+3+...+n,重点面试

--- Algorithom/src/com/niuke/Niuke46_1.java
+++ Algorithom/src/com/niuke/Niuke46_1.java
@@ -5,7 +5,7 @@ package com.niuke;
  * ²©¿ÍÎ»ÖÃ£ºhttps://my.oschina.net/liyurong/blog/1648786
  * created by liyurong
  **/
-public class Niuke48_1 {
+public class Niuke46_1 {
     public static void main(String[] args) {
         System.out.println(Sum_Solution(5));
         System.out.println(Sum_Solution(10));
