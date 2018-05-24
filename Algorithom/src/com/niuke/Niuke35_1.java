e4c2f64e014e7507c8b8d6fb6ca93e9c888b4aa1
LYR612556 <1057281882@qq.com>
2018-05-24 11:04:55 +0800

    第一个只出现一次的字符

--- Algorithom/src/com/niuke/Niuke35_1.java
+++ Algorithom/src/com/niuke/Niuke35_1.java
@@ -1,6 +1,23 @@
+package com.niuke;
+
 /**
+ * 第一个只出现一次的字符
  * created by liyurong
  **/
 public class Niuke35_1 {
-
+    public static void main(String[] args) {
+        System.out.println(FirstNotRepeatingChar("abac"));
+    }
+    public static int FirstNotRepeatingChar(String str) {
+        int[] hash = new int[256];
+        for (int i = 0;i < str.length();i ++){
+            hash[str.charAt(i)] ++;
+        }
+        for (int i = 0;i < str.length();i ++){
+            if (hash[str.charAt(i)] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
