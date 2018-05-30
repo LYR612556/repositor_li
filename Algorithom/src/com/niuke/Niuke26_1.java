b636b12f854edee4515be67b83630515a3c04b8c
LYR612556 <1057281882@qq.com>
2018-05-30 21:30:00 +0800

    复杂链表的复制

---
+++ Algorithom/src/com/niuke/Niuke26_1.java
@@ -0,0 +1,39 @@
+package com.niuke;
+
+import com.source.RandomListNode;
+
+/**
+ * 复杂链表的复制
+ * 博客位置：https://my.oschina.net/liyurong/blog/1545380
+ * created by liyurong
+ **/
+public class Niuke26_1 {
+    public static RandomListNode Clone(RandomListNode pHead) {
+        if (pHead == null) return null;
+        RandomListNode cur = pHead;
+        while (cur != null){
+            RandomListNode tmp = new RandomListNode(cur.label);
+            tmp.next = cur.next;
+            cur.next = tmp;
+            cur = tmp.next;
+        }
+        cur = pHead;
+        while (cur != null){
+            if (cur.random != null){
+                cur.next.random = cur.random.next;
+            }
+            cur = cur.next.next;
+        }
+        cur = pHead;
+        RandomListNode head = pHead.next;
+        while (cur != null){
+            RandomListNode tmp = cur.next;
+            cur.next = tmp.next;
+            if (tmp.next != null){
+                tmp.next = tmp.next.next;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
