4ed9be22525bcf3ba16512fe0768a1c8e5297935
LYR612556 <1057281882@qq.com>
2018-06-04 17:43:44 +0800

    替换空格

---
+++ Algorithom/src/com/niuke/Niuke4_1.java
@@ -0,0 +1,26 @@
+package com.niuke;
+
+/**
+ * Ìæ»»¿Õ¸ñ
+ * created by liyurong
+ **/
+public class Niuke4_1 {
+    public static void main(String[] args) {
+        System.out.println(replaceSpace(new StringBuffer("We Are Happy")));
+    }
+    public static String replaceSpace(StringBuffer str) {
+        if (str == null || str.length() == 0) {
+            return "";
+        }
+        String str1 = str.toString();
+        StringBuilder res = new StringBuilder();
+        for (int i = 0;i < str1.length();i ++){
+            if (str1.charAt(i) == ' '){
+                res.append("%20");
+            }else {
+                res.append(str1.charAt(i));
+            }
+        }
+        return res.toString();
+    }
+}
