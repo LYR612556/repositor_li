96e713878a3f430cf43292f9a1ebb777fc85b941
LYR612556 <1057281882@qq.com>
2018-06-04 15:36:29 +0800

    调整数组顺序使奇数位于偶数前面

--- Algorithom/src/com/niuke/Niuke14_1.java
+++ Algorithom/src/com/niuke/Niuke14_1.java
@@ -1,7 +1,43 @@
+package com.niuke;
+
+import java.util.Arrays;
+
 /**
  * 调整数组顺序使奇数位于偶数前面
  * created by liyurong
  **/
 public class Niuke14_1 {
-
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6};
+        reOrderArray(array);
+        for (int n : array){
+            System.out.print(n + " ");
+        }
+    }
+    public static void reOrderArray(int [] array) {
+        if (array == null || array.length == 0) return;
+        int start = 0;
+        int end = array.length - 1;
+        while (start < end){
+            while (start < end && (array[start] & 1) == 1){//奇数
+                start ++;
+            }
+            while (start < end && (array[end] & 1) == 0){
+                end --;
+            }
+            int tmp = array[start];
+            array[start] = array[end];
+            array[end] = tmp;
+        }
+        start = 0;
+        while (start < end && (array[start] & 1) == 1){
+            start ++;
+        }
+        /**
+         * @param fromIndex the index of the first element, inclusive, to be sorted
+         * @param toIndex the index of the last element, exclusive, to be sorted
+         */
+        Arrays.sort(array,0,start);
+        Arrays.sort(array,start,array.length);
+    }
 }
