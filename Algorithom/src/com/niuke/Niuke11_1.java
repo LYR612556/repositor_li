ab041a375f28be92264fb3e3668e3cd6c113e4c9
LYR612556 <1057281882@qq.com>
2018-06-04 16:37:28 +0800

    矩形覆盖~

---
+++ Algorithom/src/com/niuke/Niuke11_1.java
@@ -0,0 +1,23 @@
+package com.niuke;
+
+/**
+ * ¾ØÐÎ¸²¸Ç
+ * ²©¿ÍÎ»ÖÃ£ºhttps://my.oschina.net/liyurong/blog/1823947
+ * created by liyurong
+ **/
+public class Niuke11_1 {
+    public static void main(String[] args) {
+        System.out.println(RectCover(3));
+    }
+    public static int RectCover(int target) {
+        if (target <= 1) return target;
+        int one = 1;
+        int two = 1;
+        for (int i = 2;i <= target;i ++){
+            int tmp = one + two;
+            one = two;
+            two = tmp;
+        }
+        return two;
+    }
+}
