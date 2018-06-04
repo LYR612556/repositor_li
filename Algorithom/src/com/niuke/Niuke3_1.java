47b747f2c10b97440da3847e68e01af4764b9494
LYR612556 <1057281882@qq.com>
2018-06-04 18:08:02 +0800

    二维数组中的查找

--- Algorithom/src/com/niuke/Niuke3_1.java
+++ Algorithom/src/com/niuke/Niuke3_1.java
@@ -1,9 +1,29 @@
 package com.niuke;
 
 /**
- * 二维数组中的查找
+ * 二维数组中的查找,右上角
  * created by liyurong
  **/
 public class Niuke3_1 {
-
+    public static void main(String[] args) {
+        int[][] array = {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
+        System.out.println(Find(7,array));
+    }
+    public static boolean Find(int target, int [][] array) {
+        if (array == null || array.length == 0 || array[0].length == 0) return false;
+        int m = array.length;
+        int n = array[0].length;
+        int i = 0;
+        int j = n - 1;
+        while (i < m && j >= 0){
+            if (array[i][j] == target){
+                return true;
+            }else if (array[i][j] < target){
+                i ++;
+            }else {
+                j --;
+            }
+        }
+        return false;
+    }
 }
