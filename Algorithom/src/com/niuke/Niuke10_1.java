a785d37c8734542abb11503407322b5329253b3e
LYR612556 <1057281882@qq.com>
2018-06-04 16:40:34 +0800

    https://my.oschina.net/liyurong/blog/1630647

---
+++ Algorithom/src/com/niuke/Niuke10_1.java
@@ -0,0 +1,15 @@
+package com.niuke;
+
+/**
+ * 变态跳台阶
+ * 博客位置：https://my.oschina.net/liyurong/blog/1630647
+ * created by liyurong
+ **/
+public class Niuke10_1 {
+    public static void main(String[] args) {
+        System.out.println(3);
+    }
+    public static int JumpFloorII(int target) {
+        return (int)Math.pow(2,target - 1);
+    }
+}
